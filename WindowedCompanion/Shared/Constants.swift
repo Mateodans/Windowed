8bad1b40f5dfd53ebbc46e520d22ed67fa52ad92
Mateo Dans <mateodans@gmail.com>
2026-09-13 23:09:40 -0300

    chore: Bump project version to 1.1 (build 1) across iOS and macOS targets

--- WindowedCompanion/Shared/Constants.swift
+++ WindowedCompanion/Shared/Constants.swift
@@ -5,5 +5,5 @@ public enum Constants {
     public static let maxFrameSize: Int = 16_777_216 // 16MB
     public static let keychainService = "com.windowed.companion.pairing"
     public static let appName = "Windowed Companion"
-    public static let appVersion = "1.0.0"
+    public static let appVersion = "1.1.0"
 }
